86047363a07e6899d4c2062f03b496eff51f475a
Eli Sampson <elisampsonj@gmail.com>
2024-07-31 14:06:08 -0600

    renamed stuff

--- letterbox_solver/main.py
+++ letterbox_solver/main.py
@@ -3,211 +3,81 @@ import lb_functions as lbf
 import sys
 
 
-def solver(letters, strategy=lbf.get_score3): #letters should be a list containing 4 lists
-    words = lbf.get_possible_words(letters)
-    sequence = []
-    
 
-    first = True
-    letter_set = set(letters)   
-    while letter_set:
-        # print(letter_set)
-        if first:
-            score_dict = {word: strategy(word,letter_set) for word in words}
-            
-            ranked = lbf.order(score_dict)
-            first = False
-            
-            #choose top word
-            chosen = ranked[0]
-            words.remove(chosen)
-            
-            sequence.append(chosen)
-
-            #update letter_set
-            for l in (letter_set & set(chosen)):
-                letter_set.remove(l)
-                
-                
-            
-        else:
-            #filter out words that don't start with starting_letter
-            filtered_words = filter(lambda word: chosen[-1] == word[0], words)
-            
-
-            score_dict = {word: strategy(word,letter_set) for word in filtered_words}
-            ranked = lbf.order(score_dict)
-
-            if len(ranked) == 0:
-                return False
-            chosen = ranked[0]
-            words.remove(chosen)
-            
-            sequence.append(chosen)
-
-            #update letter_set
-            for l in (letter_set & set(chosen)):
-                letter_set.remove(l)
-                
-
-    # print(sequence)
-    return sequence
-
-
-
-####################################
-#different strategy
-####################################
-
-
-
-
-
-
-def checkleft(words, chosen, letter_set,  check_left):
-    if check_left:
-        left_words = filter(lambda word: chosen[0] == word[-1], words)
-        left_dict = {word: lbf.get_score1(word,letter_set) for word in left_words}
-        left_ranked = lbf.order(left_dict)
-        if len(left_ranked) == 0:
-            return None
-
-
-        best_left = left_ranked[0]
-
-        return best_left
-    else:
-        return None
-    
-
-def checkright(words, chosen, letter_set,  check_right):
-    if check_right:
-        right_words = filter(lambda word: chosen[-1] == word[0], words)
-        right_dict = {word: lbf.get_score1(word,letter_set) for word in right_words}
-        right_ranked = lbf.order(right_dict)
-        if len(right_ranked) == 0:
-            return None
-
-        best_right = right_ranked[0]
-
-        return best_right
-    else:
-        return None
-
-
-
-        
-
-def solver2(letters):
-    #get all possible words
-    words = lbf.get_possible_words(letters)
-    sequence = []
-    letter_set = set(letters)
-
-    #chose the best word
+def chosebest(words, letter_set, i):
     score_dict = {word: lbf.get_score1(word,letter_set) for word in words}  
     ranked = lbf.order(score_dict)
-    print(ranked)
-    chosen = ranked[2]
-    sequence.append(chosen)
-    words.remove(chosen)
+    
+    if len(ranked) == 0:
+        return None
+    
+    chosen = ranked[i]
 
-    #update letter_set
+    return chosen
+
+
+def update_letters(letter_set, chosen):
     for l in (letter_set & set(chosen)):
         letter_set.remove(l)
+    return letter_set
 
 
 
-
-    check_left, check_right = True, True
-    while letter_set:
-        #chose the best left and right
-        best_left = checkleft(words, chosen, letter_set, check_left)
-        best_right = checkright(words, chosen, letter_set, check_right)
-
-        #you dont need to compare when you arent even making a choice
-        #check if we have already chosen a direction
+def checkleft(words, chosen, letter_set):
+    left_words = filter(lambda word: chosen[0] == word[-1], words)
+    return chosebest(left_words, letter_set, 0)
+    #bestleft is either the best left word or None if there are no words to the left
 
 
-        #if we need to compare the choices
-        if check_left and check_right:
-            #if left is better
-            if len((letter_set & set(best_left))) > len((letter_set & set(best_right))):
-                sequence.insert(0, best_left)
-                words.remove(best_left)
-
-                #update letter_set
-                for l in (letter_set & set(best_left)):
-                    letter_set.remove(l)
-                
-                chosen = best_left
-                check_right = False
+def checkright(words, chosen, letter_set):
+    right_words = filter(lambda word: chosen[-1] == word[0], words)
+    return chosebest(right_words, letter_set, 0)
+    #bestright is either the best right word or None if there are no words to the right
 
 
-            else: 
-                sequence.append(best_right)
-                words.remove(best_right)
 
-                #update letter_set
-                for l in (letter_set & set(best_right)):
-                    letter_set.remove(l)
-                
-                chosen = best_right
-                check_left = False
+def checkdone(letter_set, root, addition):
+    if len(letter_set) == len(letter_set & set(root + addition)):
+        return True
+    else:
+        return False
+    
 
-        #we don't need to compare
-        elif check_left:
-            sequence.insert(0, best_left)
-            words.remove(best_left)
-            
-            #update letter_set
-            for l in (letter_set & set(best_left)):
-                letter_set.remove(l)    
-            chosen = best_left
+
+
+def iterate(letters):
+    #get all spellable words
+    words = lbf.get_possible_words(letters)
+    letter_set = set(letters)
+
+    for i in range(1000):
+        #chose the ith best word
+        root = chosebest(words,letter_set,i)
+        sequence = [root]
+
+
+        #find best left
+        bestleft = checkleft(words,root,letter_set)
+        if bestleft:
+            if checkdone(letter_set,root,bestleft):
+                sequence.insert(0, bestleft)
+                return sequence, i
+
+
+
         
-        elif check_right:
-                sequence.append(best_right)
-                words.remove(best_right)
-
-                #update letter_set
-                for l in (letter_set & set(best_right)):
-                    letter_set.remove(l)
-                chosen = best_right
-
-
-
-
-            
-
-
-
-
-        # print(f"best: {chosen}")
-        # print(f"left: {best_left}")
-        # print(f"right: {best_right}")
-        # print(f"best sequence is: {sequence}")
-        # print(letter_set)
+        #find the best right
+        bestright = checkright(words, root, letter_set)
+        if bestright:
+            if checkdone(letter_set, root, bestright):
+                sequence.append(bestright)
+                return sequence, i
     
-    
-       
-    return sequence
-
-    
-
-    
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
     letters = sys.argv[1]
-    strategy = sys.argv[2]
-    dumb = {'1': lbf.get_score1,'2': lbf.get_score2, '3': lbf.get_score3, '4': lbf.get_score4 }
-    #print(solver(letters, dumb[strategy]))
+    print(iterate(letters))
 
-    print(solver2(letters))
+
+
